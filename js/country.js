a7f493b0bd62560877670a3953c3176e9d06e1aa
shimislm <shimislm5@gmail.com>
2022-08-29 13:05:47 +0300

    filter fixed

--- js/country.js
+++ js/country.js
@@ -7,7 +7,7 @@ export default class Country {
         this.borders = _item.borders ? _item.borders : "No Borders";
         let corrency = Object.values(_item.currencies)
         this.currency = `${corrency[0].name} ${corrency[0].symbol}`;
-        this.flag = _item.flags.svg;
+        this.flag = _item.flags.png;
         this.pop = `${(Math.floor((_item.population / 1000000) * 100) / 100).toLocaleString()}M`;
         this.capital = _item.capital
         let languages = Object.values(_item.languages)
