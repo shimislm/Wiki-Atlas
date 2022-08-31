b783bc16e76251b49f630b62e534ad9de9a718a0
shimislm <shimislm5@gmail.com>
2022-08-31 08:29:36 +0300

    ui/ux fixes

--- js/country.js
+++ js/country.js
@@ -1,5 +1,4 @@
 import {createCommonCountries, createSingleCountry, displayBorderName } from "./countriesManager.js";
-
 export default class Country {
     constructor(_parent, _item) {
         this.parent = _parent;
@@ -14,7 +13,6 @@ export default class Country {
         this.languages = languages;
         this.region = _item.region;
         this.latlng = _item.latlng;
-        // console.log(this.latlng)
     }
     /**create first common cuntries*/
     renderCommon() {
@@ -22,16 +20,12 @@ export default class Country {
         let myDiv = document.createElement("div");
         myDiv.className = "country justify-content-between h-100 p-0";
         document.querySelector(this.parent).append(myDiv);
-        myDiv.innerHTML += `
-        <div class="country-box bg-light border border-2 mx-md-2  rounded-4 opacity-100 text-dark h-100 p-2">
+        myDiv.innerHTML +=`<div class="country-box bg-light border border-2 mx-md-2  rounded-4 opacity-100 text-dark h-100 p-2">
         <h2 class=" text-center mb-1 rounded-4" >Name: ${this.name}</h2>
         <div class="h-75 flag rounded-4 m-2 shadow" style="background-image:url(${this.flag}) ;">
         </div>
         <div class="h-50">
-        <h4>Capital: ${this.capital}</h4>
-        </div>
-
-    </div>`
+        <h4>Capital: ${this.capital}</h4></div></div>`
         let box = myDiv.querySelector(".country-box")
         box.addEventListener("click", () => {
             parent.innerHTML = "";
@@ -46,8 +40,7 @@ export default class Country {
         myDiv.className = "country w-100 row my-2 justify-content-between p-0";
         // myDiv.style = "cursor: none;"
         document.querySelector(this.parent).append(myDiv);
-        myDiv.innerHTML += `
-        <div class="country-box bg-light mx-md-0 text-dark col-md-5 py-2 d-flex flex-column align-items-start" style="cursor: default;">
+        myDiv.innerHTML += `<div class="country-box bg-light mx-md-0 text-dark col-md-5 py-2 d-flex flex-column align-items-start" style="cursor: default;">
         <h2 class=" text-center mb-1 rounded-4" >Name: ${this.name}</h2>
         <img class="rounded-4 align-self-center shadow m-2" src="${this.flag}" alt="${this.name}" width="100%">
         <h4>Capital: ${this.capital}</h4>
@@ -56,24 +49,12 @@ export default class Country {
         <h6>Languages: ${this.languages}</h6>
         <h6>Currency: ${this.currency} </h6>
         <h6>Region: ${this.region} </h6>
-        <button class="btn btn-primary mt-2 text-end">Back</button>
-        </div>
-        <div class="map border border-white border-2 col-md-6 my-md-2 my-3 rounded-4 p-0 overflow-hidden">
-        </div>
-        `
+        <button class="btn btn-primary mt-2 text-end">Back</button></div>
+        <div class="map border border-white border-2 col-md-6 my-md-2 my-3 rounded-4 p-0 overflow-hidden"></div>`
+        // render the map
         let map = myDiv.querySelector(".map")
         map.className = "country-box p-0 rounded-end-4 text-dark col-md-7 p-0"
-        map.innerHTML = `
-        <iframe 
-            width="100%" 
-            height="100%
-            frameborder="0" 
-            scrolling="no" 
-            marginheight="0" 
-            marginwidth="0" 
-            src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&hl=en&z=5&amp;output=embed"
-            >
-        </iframe>
+        map.innerHTML = `<iframe width="100%" height="100% frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=${this.latlng[0]},${this.latlng[1]}&hl=en&z=5&amp;output=embed"></iframe>
         `
         // create back button
         let btn = myDiv.querySelector(".btn")
